80093fbaaa634268f11f70bc627fe18c7e8212f1
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-03-15 14:05:35 +0200

    VarausHallinta

--- Hotellitehtava/Hotellitehtava/VaraustenHallinta.Designer.cs
+++ Hotellitehtava/Hotellitehtava/VaraustenHallinta.Designer.cs
@@ -28,12 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.uusiVarausBT = new System.Windows.Forms.Button();
+            this.tyhjennaVarausKenttaBT = new System.Windows.Forms.Button();
+            this.poistaVarausBT = new System.Windows.Forms.Button();
+            this.muokkaaVarausBT = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1203, 182);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox1.Font = new System.Drawing.Font("Miriam Libre", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(365, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 65);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Varausten hallinta";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Varaus nro:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 492);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ulos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(103, 435);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sisään:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Huoneen nro:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Huonetyyppi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Miriam Libre", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(47, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 29);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Asiakas nro:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(228, 263);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 28);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(228, 379);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(142, 28);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(228, 320);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(142, 28);
+            this.comboBox4.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(228, 437);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(142, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(228, 495);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(142, 26);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(421, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(771, 561);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // uusiVarausBT
+            // 
+            this.uusiVarausBT.Location = new System.Drawing.Point(12, 569);
+            this.uusiVarausBT.Name = "uusiVarausBT";
+            this.uusiVarausBT.Size = new System.Drawing.Size(155, 38);
+            this.uusiVarausBT.TabIndex = 15;
+            this.uusiVarausBT.Text = "Lisää uusi varaus";
+            this.uusiVarausBT.UseVisualStyleBackColor = true;
+            // 
+            // tyhjennaVarausKenttaBT
+            // 
+            this.tyhjennaVarausKenttaBT.Location = new System.Drawing.Point(215, 643);
+            this.tyhjennaVarausKenttaBT.Name = "tyhjennaVarausKenttaBT";
+            this.tyhjennaVarausKenttaBT.Size = new System.Drawing.Size(155, 38);
+            this.tyhjennaVarausKenttaBT.TabIndex = 16;
+            this.tyhjennaVarausKenttaBT.Text = "Tyhjennä kentät";
+            this.tyhjennaVarausKenttaBT.UseVisualStyleBackColor = true;
+            // 
+            // poistaVarausBT
+            // 
+            this.poistaVarausBT.Location = new System.Drawing.Point(279, 569);
+            this.poistaVarausBT.Name = "poistaVarausBT";
+            this.poistaVarausBT.Size = new System.Drawing.Size(91, 38);
+            this.poistaVarausBT.TabIndex = 17;
+            this.poistaVarausBT.Text = "Poista";
+            this.poistaVarausBT.UseVisualStyleBackColor = true;
+            // 
+            // muokkaaVarausBT
+            // 
+            this.muokkaaVarausBT.Location = new System.Drawing.Point(173, 569);
+            this.muokkaaVarausBT.Name = "muokkaaVarausBT";
+            this.muokkaaVarausBT.Size = new System.Drawing.Size(100, 38);
+            this.muokkaaVarausBT.TabIndex = 18;
+            this.muokkaaVarausBT.Text = "Muokkaa";
+            this.muokkaaVarausBT.UseVisualStyleBackColor = true;
+            // 
+            // VaraustenHallinta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1206, 775);
+            this.Controls.Add(this.muokkaaVarausBT);
+            this.Controls.Add(this.poistaVarausBT);
+            this.Controls.Add(this.tyhjennaVarausKenttaBT);
+            this.Controls.Add(this.uusiVarausBT);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "VaraustenHallinta";
             this.Text = "VaraustenHallinta";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button uusiVarausBT;
+        private System.Windows.Forms.Button tyhjennaVarausKenttaBT;
+        private System.Windows.Forms.Button poistaVarausBT;
+        private System.Windows.Forms.Button muokkaaVarausBT;
     }
 }
