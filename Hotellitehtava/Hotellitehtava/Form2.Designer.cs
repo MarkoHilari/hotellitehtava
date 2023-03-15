a5b24a28f536dc6211e0ed601d2aac0adf802ce7
mika huotari <mika.huotari@edu.keuda.fi>
2023-03-15 13:11:00 +0200

    asiakkaiden

--- Hotellitehtava/Hotellitehtava/Form2.Designer.cs
+++ Hotellitehtava/Hotellitehtava/Form2.Designer.cs
@@ -28,26 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(paaikkuna));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.henkiloMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.varausMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.HuoneMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // menuStrip1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1516, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.menuStrip1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.henkiloMS,
+            this.varausMS,
+            this.HuoneMS});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1516, 46);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // henkiloMS
+            // 
+            this.henkiloMS.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.henkiloMS.Name = "henkiloMS";
+            this.henkiloMS.Size = new System.Drawing.Size(341, 42);
+            this.henkiloMS.Text = "Hallitse henkilökunta";
+            // 
+            // varausMS
+            // 
+            this.varausMS.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.varausMS.Name = "varausMS";
+            this.varausMS.Size = new System.Drawing.Size(290, 42);
+            this.varausMS.Text = "Hallitse varauksia";
+            // 
+            // HuoneMS
+            // 
+            this.HuoneMS.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HuoneMS.Name = "HuoneMS";
+            this.HuoneMS.Size = new System.Drawing.Size(279, 42);
+            this.HuoneMS.Text = "Hallitse huoneita";
             // 
             // paaikkuna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1516, 995);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "paaikkuna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pääikkuna";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +93,9 @@
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem henkiloMS;
+        private System.Windows.Forms.ToolStripMenuItem varausMS;
+        private System.Windows.Forms.ToolStripMenuItem HuoneMS;
     }
 }
